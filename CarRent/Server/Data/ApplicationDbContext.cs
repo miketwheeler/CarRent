0ffd0e99462db7e961220c52da7019f4cb2dc2de
miketwheeler <mikeytwheeler@gmail.com>
2021-06-15 19:24:46 -0700

    Add project files.

---
+++ CarRent/Server/Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using CarRent.Server.Models;
+using IdentityServer4.EntityFramework.Options;
+using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarRent.Server.Data
+{
+    public class ApplicationDbContext : ApiAuthorizationDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(
+            DbContextOptions options,
+            IOptions<OperationalStoreOptions> operationalStoreOptions) : base(options, operationalStoreOptions)
+        {
+        }
+    }
+}
