c5a542b720d260857b9335d08190c40a44e6935c
miketwheeler <mikeytwheeler@gmail.com>
2021-06-16 11:37:57 -0700

    migrated database and fixed some errors

---
+++ CarRent/Server/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using CarRent.Server.Data;
+using CarRent.Server.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(CarRent.Server.Areas.Identity.IdentityHostingStartup))]
+namespace CarRent.Server.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
